08462eb42133475b3b2c8c2457448c73a03f2614
anas <17gcs23@meaec.edu.in>
2021-10-12 14:21:38 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [ascending, setAscending] = useState(true);
+  const [counterValue, setCounterValue] = useState(0);
+
+  const ascendingCounter = () => {
+    setCounterValue((value) => value + 1);
+  };
+
+  const descendingCounter = () => {
+    setCounterValue((value) => value - 1);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => (ascending ? ascendingCounter() : descendingCounter()),
+      1000
+    );
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [ascending]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 25 }}>Counter Value: {counterValue}</Text>
+      <Button title="Reverse" onPress={() => setAscending((value) => !value)} />
     </View>
   );
 }
@@ -14,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
